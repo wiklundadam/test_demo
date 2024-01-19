45f1010550a3aa59fe28c6012cc75bfaa95b019b
wiklundadam <wiklund.adamdam@gmail.com>
2024-01-19 19:15:42 +0100

    testfile upload

---
+++ test3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int val1 = 10;
+    int val2 = 12;
+
+    int sum = val1 + val2;
+
+    cout << "Hello GITHUB: " << sum << endl;
+
+}
